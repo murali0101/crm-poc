bbe788677d2dfc36b08aa53ba8b49b1f533548ff
murali mv <muralimv0101@gmail.com>
2025-10-15 22:25:27 +0530

    initial react vite typescript & tailwind css code setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+     <Fragment>
+           <div className='min-h-screen flex justify-center items-center'>
+             <h1 className='text-blue-500 text-5xl'  >test</h1> 
+           </div>
+     </Fragment>
+  )
+}
+
+export default App
